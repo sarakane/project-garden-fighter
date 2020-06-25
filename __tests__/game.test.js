22022f41cb25f18484cfd28aaf64390b3a77f93d
Sara Kane <kane.sarak@gmail.com>
2020-06-25 15:14:54 -0700

    Add passing test for 'The program should increase a characters experience if they win a battle'
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,17 +1,19 @@
 import { Game } from './../src/Game.js';
 import { Character } from './../src/Character.js';
 import { Weapon } from './../src/Weapon.js';
-import { Monster } from '../src/Monster.js';
+import { Monster, Slug } from '../src/Monster.js';
 
 describe ('Game', () => {
   let fighter;
   let game;
   let monster;
+  let slug;
   
   beforeEach(() => {
     fighter = new Character("Trogdor", "Fighter", 5, 1, 20);
     game = new Game(fighter);
     monster = new Monster("mole", 15, 20);
+    slug = new Slug("green");
   });
 
   test('The program should create a new game with a new character based on the name and character class chosen by the user', () => {
@@ -26,11 +28,13 @@ describe ('Game', () => {
   test('The program should remove health from a character if they lose a battle', () => {
     game.giveCharacterStartingWeapon();
     game.battleMonster(monster);
-    console.log(game.character);
     expect(game.character.health).toEqual(16);
   })
 
   test('The program should increase a characters experience if they win a battle', () => {
-
-  })
+    game.giveCharacterStartingWeapon();
+    game.battleMonster(slug);
+    console.log(game.character);
+    expect(game.character.experience).toEqual(5);
+  });
 });
