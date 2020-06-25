cf210062061e7a4390d4fac405909498a52e128f
Sara Kane <kane.sarak@gmail.com>
2020-06-25 13:54:29 -0700

    Add failing test for 'The program should give a new character a starting weapon based on the characters class'
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -12,6 +12,10 @@ describe ('Game', () => {
 
   test('The program should create a new game with a new character based on the name and character class chosen by the user', () => {
     expect(game.character.charClass).toEqual("Fighter");
-    console.log(game);
+  });
+
+  test('The program should give a new character a starting weapon based on the characters class', () => {
+    game.giveCharacterStartingWeapon();
+    expect(game.character.equipped).toEqual(expect.arrayContaining([{name: "Shovel", strength: 5, magic: 0}]));
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -6,18 +6,18 @@ export class Game {
     this.character = character;
   }
   giveCharacterStartingWeapon() {
-    if(this.character.charClass === "Fighter"){
-      let shovel = new Weapon("shovel", 5, 0);
+/*     if(this.character.charClass === "Fighter"){
+      let shovel = new Weapon("Shovel", 5, 0);
       this.character.addItem(shovel);
-      this.character.equipItem(shovel);
+      this.character.equipWeapon(shovel);
     } else if (this.character.charClass === "Wizard") {
       let wateringCan = new Weapon("Watering Can", 0, 5);
       this.character.addItem(wateringCan);
-      this.character.equipItem(wateringCan);
+      this.character.equipWeapon(wateringCan);
     } else if (this.character.charClass === "Rogue") {
       let spade = new Weapon("Spade", 3, 3);
       this.character.addItem(spade);
-      this.character.equipItem(spade);
-    }
+      this.character.equipWeapon(spade);
+    } */
   }
 };
